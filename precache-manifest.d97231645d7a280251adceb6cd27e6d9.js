0897df94eac820668b813ff094fe102cc8a60d24
JuliaDzyuba <newcom1000.2017@gmail.com>
2020-09-30 12:20:47 +0300

    Updates

---
+++ precache-manifest.d97231645d7a280251adceb6cd27e6d9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda1340f06940396389d04c9459b3660",
+    "url": "/jdwebdev/index.html"
+  },
+  {
+    "revision": "c6f1d3b8a7f10e703177",
+    "url": "/jdwebdev/static/css/main.dc19603d.chunk.css"
+  },
+  {
+    "revision": "93c2e5d0c8bec525adcf",
+    "url": "/jdwebdev/static/js/2.c0717d7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jdwebdev/static/js/2.c0717d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6f1d3b8a7f10e703177",
+    "url": "/jdwebdev/static/js/main.e4d48c97.chunk.js"
+  },
+  {
+    "revision": "941e928309372eb2ccb4",
+    "url": "/jdwebdev/static/js/runtime-main.6d40cb5d.js"
+  },
+  {
+    "revision": "6a6a61e827c6bde5e1b128eb76f66168",
+    "url": "/jdwebdev/static/media/Wallpaper.6a6a61e8.png"
+  },
+  {
+    "revision": "92507c8a6888c9937dc6feeb1fb7adc2",
+    "url": "/jdwebdev/static/media/instagram.92507c8a.svg"
+  },
+  {
+    "revision": "d098c93ffeeef27fef1d8c67f7e51dcb",
+    "url": "/jdwebdev/static/media/linkedin.d098c93f.svg"
+  },
+  {
+    "revision": "8d859602e4edf37f8e6fbce188479840",
+    "url": "/jdwebdev/static/media/react.8d859602.png"
+  }
+]);
